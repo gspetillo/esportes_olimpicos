af0decf0fb1c07193866b64ed35516fcd2fc7d5b
Gabriel Petillo <32914162+gspetillo@users.noreply.github.com>
2020-04-20 12:39:49 -0300

    Criação de diretórios e arquivos iniciais

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Gabriel Petillo\Documents\Faculdades\FIAP\3SIA - 2020\Desenvolvimento Cross Platform\NAC1\gitt\esportes_olimpicos"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Flutter\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
